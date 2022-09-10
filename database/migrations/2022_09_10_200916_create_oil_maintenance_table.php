1f23cb5f5feacfe5f7c8433b3282b7c95145c72c
Wil <wil@Wilsons-MacBook-Pro-2.local>
2022-09-10 22:43:26 +0100

    create database migrations

--- database/migrations/2022_09_10_200916_create_oil_maintenance_table.php
+++ database/migrations/2022_09_10_200916_create_oil_maintenance_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('oil_maintenance', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('generator_id');
+            $table->date('changed_oil_on');
+            $table->foreign('generator_id')->references('id')->on('generators')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('oil_maintenance');
     }
 };
--- database/migrations/2022_09_10_201101_create_running_logs_table.php
+++ database/migrations/2022_09_10_201101_create_running_logs_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('running_logs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId('generator_id');
+            $table->date('ran_on');
+            $table->tinyInteger('hours_ran');
+            $table->foreign('generator_id')->references('id')->on('generators')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('running_logs');
     }
 };

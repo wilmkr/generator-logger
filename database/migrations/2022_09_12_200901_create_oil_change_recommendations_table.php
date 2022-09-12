aaf8218a283d5224fa9fffd5d236a954a15c4c39
Wil <wil@Wilsons-MacBook-Pro-2.local>
2022-09-12 21:17:42 +0100

    modify migration and create models

--- database/migrations/2022_09_12_200901_create_oil_change_recommendations_table.php
+++ database/migrations/2022_09_12_200901_create_oil_change_recommendations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('oil_change_recommendation', function (Blueprint $table) {
+        Schema::create('oil_change_recommendations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('generator_id');
             $table->smallInteger('hours_interval');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('oil_change_recommendation');
+        Schema::dropIfExists('oil_change_recommendations');
     }
 };
